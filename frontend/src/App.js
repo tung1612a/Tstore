43ffdab7cce9fbc7e2c9007e73f1e982ca23671d
ThinhQT <thinhqths171406@fpt.edu.vn>
2025-10-17 11:33:14 +0700

    ForgetPass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import ProductDetail from "./components/Product/ProductDetail"
+import ForgotPassword from "./components/Login/ForgotPassword";
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/forgetPass" element={<ForgotPassword />}></Route>
       </Routes>
     </BrowserRouter>
   )
